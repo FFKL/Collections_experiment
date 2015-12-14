ed91bff589f4dc2584c7836502506c0646525005
dmitriy <FFKL@yandex.ru>
2015-12-14 23:40:10 +0200

    JavaDoc for User, UserTest

--- src/main/java/org/collectionsexp/collectionsexp/User.java
+++ src/main/java/org/collectionsexp/collectionsexp/User.java
@@ -1,23 +1,27 @@
 package org.collectionsexp.collectionsexp;
 
 public class User {
-
     final int id;
     final String name;
     final int age;
     final boolean sex;
-
+    /**
+     * Конструктор
+     * @param id ID пользователя
+     * @param name имя пользователя
+     * @param age возраст пользователя
+     * @param sex пол пользователя
+     */
     public User(final int id, final String name, final int age, final boolean sex) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.sex = sex;
     }
-
-    public int getId() {
-        return id;
-    }
-
+    /**
+     * Переопределение метода toString()
+     * @return возвращение строки в виде ID, name, age, sex
+     */
     @Override
     public String toString() {
         return " ID: " + id +
@@ -26,6 +30,11 @@ public class User {
                 " Пол: " + (sex ? "Мужской" : "Женский");
     }
 
+    /**
+     * Переопределение метода equals()
+     * @param obj объект для сравнения
+     * @return результат сравнения
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -51,6 +60,10 @@ public class User {
         return true;
     }
 
+    /**
+     * Переопределение метода hashCode()
+     * @return hash code
+     */
     @Override
     public int hashCode() {
         int result = 55;
