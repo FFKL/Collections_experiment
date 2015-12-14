1a35696ecffed493d62ce373df21d6c0838bf44c
dmitriy <FFKL@yandex.ru>
2015-12-14 23:37:59 +0200

    JavaDoc for User, UserTest

--- src/test/java/org/collectionsexp/collectionsexp/UserTest.java
+++ src/test/java/org/collectionsexp/collectionsexp/UserTest.java
@@ -5,13 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UserTest {
-
+    /**
+     * user.equals(user)
+     */
     @Test
     public void reflexive() {
         User user = (new User(1, "Дима", 22, true));
         assertEquals(user, user);
     }
-
+    /**
+     * user1.equals(user2), user2.equals(user1)
+     */
     @Test
     public void symmetric() {
         User user1 = (new User(1, "Дима", 22, true));
@@ -19,7 +23,9 @@ public class UserTest {
         assertEquals(user1, user2);
         assertEquals(user2, user1);
     }
-
+    /**
+     * user1.equals(user2), user2.equals(user3), user3.equals(user1)
+     */
     @Test
     public void transitive() {
         User user1 = (new User(1, "Дима", 22, true));
@@ -29,7 +35,9 @@ public class UserTest {
         assertEquals(user2, user3);
         assertEquals(user1, user3);
     }
-
+    /**
+     * user1.equals(user2) true в 10 итерациях
+     */
     @Test
     public void consistant() {
         User user1 = (new User(1, "Дима", 22, true));
@@ -37,5 +45,4 @@ public class UserTest {
         for (int i = 0; i < 10; ++i)
             assertEquals(user1, user2);
     }
-
 }
